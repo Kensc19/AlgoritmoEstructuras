15a33a93c6d136d7ecca3aa6702ea109908b8e97
bulan506 <116318596+bulan506@users.noreply.github.com>
2023-04-23 10:03:02 -0600

    Cho

--- src/Lista.java
+++ src/Lista.java
@@ -3,16 +3,13 @@ import java.util.Scanner;
 
 public class Lista {
     Scanner options = new Scanner(System.in);
-    Estudiante estudiante = new Estudiante();
     Estudiante inicio = new Estudiante();
     private int countEstuds;
 
     public void menu() {
-
-
         System.out.println("1 Ingresar Estudiante");
-        System.out.println("2 Mostrar todos los Estudiantes por ID\n");
-        System.out.println("3 Mostrar todos los Estudiantes por Nombre\n");
+        System.out.println("2 Mostrar todos los Estudiantes por ID");
+        System.out.println("3 Mostrar todos los Estudiantes por Nombre");
         System.out.println("4 Mostrar todos los Estudiantes con todos sus datos");
         System.out.println("5 Mostrar un solo Estudiante por ID");
         System.out.println("6 Mostrar un solo Estudiante por Nombre");
@@ -21,9 +18,6 @@ public class Lista {
         System.out.println("9 Eliminar Estudiante");
         System.out.println("10 Indicar la cantidad de Estudiantes registrados");
         System.out.println("11 Salir");
-
-
-        try {
             int optionUser = options.nextInt();
             if (optionUser != 11) {
                 switch (optionUser) {
@@ -54,9 +48,6 @@ public class Lista {
                     case 10:
 
                         break;
-                    case 11:
-
-                        break;
                     default:
                         System.out.println("\nIngresó una opción que no está en el menú");
                         break;
@@ -65,57 +56,55 @@ public class Lista {
             else {
                 System.out.println("Decidió salir");
             }
-        } catch (NullPointerException | NumberFormatException | InputMismatchException w) {
-            System.out.println("\n**********Ingrese el tipo de valor correcto ************");
-        }// end catch
-
     }
 
     public void ingresarEstds() {
+        boolean flag = true;
+        do {
+            System.out.println("\nIngrese el ID: Int");
+            int idEstuds = options.nextInt();
+            System.out.println("\nIngrese el nombre: Str");
+            options.nextLine();
+            String nombre = options.nextLine();
+            System.out.println("\nIngrese el numero de celular: Int");
+            int numCelular = options.nextInt();
+            System.out.println("\nIngrese el carnet: str");
+            options.nextLine();
+            String carnet = options.nextLine();
+            System.out.println("\nIngrese la edad: Int");
+            int edad = options.nextInt();
+            agregarAlFinal(new Estudiante(idEstuds, nombre, numCelular, carnet, edad));
+            System.out.println("\n¿ Desea ingresar más estudiantes ?: Int \n 1)_____ SÍ\n 2)______NO");
+            options.nextLine();
+            int option = options.nextInt();
+            if (option != 1) {
+                flag = false;
+            }
+        } while (flag);
+        menu();
 
-        System.out.println("\nIngrese el ID: Int");
-        int idEstuds = options.nextInt();
-        System.out.println("\nIngrese el nombre: Str");
-        options.nextLine();
-        String nombre = options.nextLine();
-        System.out.println("\nIngrese el numero de celular: Int");
-        options.nextLine();
-        int numCelular = options.nextInt();
-        System.out.println("\nIngrese el carnet: Int");
-        options.nextLine();
-        String carnet = options.nextLine();
-        System.out.println("\nIngrese la edad: Int");
-        options.nextLine();
-        int edad = options.nextInt();
     }
-
     public boolean listaEstudsVacia(){
         return inicio==null;
     }
 
     public void agregarAlFinal(Estudiante estud){
         // Define un nuevo nodo.
-        Estudiante nuevo = new Estudiante(estud.getIdEstudiante(),estud.getNomEstudiante(),estud.getNumCelular(),estud.getCarnet(),estud.getEdad());
-
-        // Consulta si la lista esta vacia.
+        Estudiante nuevo = new Estudiante(estud.getIdEstudiante(),estud.getNomEstudiante(),
+                               estud.getNumCelular(),estud.getCarnet(),estud.getEdad());
         if (listaEstudsVacia()) {
-            // Inicializa la lista agregando como inicio al nuevo nodo.
             inicio = nuevo;
-            // Caso contrario recorre la lista hasta llegar al ultimo nodo
-            //y agrega el nuevo.
-        } else{
-            // Crea ua copia de la lista.
+        } else {
             Estudiante aux = inicio;
-            // Recorre la lista hasta llegar al ultimo nodo.
-            while(aux.getSiguiente() != null){
+            while (aux.getSiguiente() != null) {
                 aux = aux.getSiguiente();
             }
-            // Agrega el nuevo nodo al final de la lista.
             aux.setSiguiente(nuevo);
         }
-
         countEstuds++;
     }
+
+
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Lista lis= new Lista();
+        lis.menu();
     }
 }
