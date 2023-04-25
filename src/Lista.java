02dbaac0e09b004d846e45c6365e4ccf7bb586f0
bulan506 <116318596+bulan506@users.noreply.github.com>
2023-04-24 22:31:46 -0600

    fijarse en modificar estd

--- src/Lista.java
+++ src/Lista.java
@@ -5,7 +5,7 @@ public class Lista {
     Scanner options = new Scanner(System.in);
     Estudiante inicio;
     Estudiante estudFinal= new Estudiante();
-    private int countEstuds;
+    private int countEstuds=0;
 
     public void menu() {
         System.out.println("1 Ingresar Estudiante");
@@ -29,9 +29,12 @@ public class Lista {
                         mostrarEstudiantesId();
                         break;
                     case 3:
-
+                        mostrarEstudiantesNombre();
                         break;
                     case 4:
+                        mostrarEstudiantes();
+                        break;
+                    case 5:
 
                         break;
                     case 6:
@@ -47,7 +50,8 @@ public class Lista {
 
                         break;
                     case 10:
-
+                        System.out.println(showCountStuds());
+                        menu();
                         break;
                     default:
                         System.out.println("\nIngresó una opción que no está en el menú");
@@ -80,8 +84,7 @@ public class Lista {
             int option = options.nextInt();
             if (option != 1) {
                 flag = !flag;
-            }
-        } while (flag);
+            }} while (flag);
         menu();
 
     }
@@ -105,9 +108,35 @@ public class Lista {
     }
     public void mostrarEstudiantesId(){
         int i=0;
+        Estudiante aux= inicio;
+        if(!listaEstudsVacia()) {
+            while (aux.getSiguiente() != null) {
+                System.out.print(i + ".[ Estudiante: " + aux.getIdEstudiante() + " ]" + " ->  ");
+                aux = aux.getSiguiente();
+                i++;
+            }
+            System.out.print( "NULL");
+        }
+    }
+    public void mostrarEstudiantesNombre(){
+        int i=0;
         if(!listaEstudsVacia()) {
             while (inicio.getSiguiente() != null) {
-                System.out.print(i + ".[ Estudiante: " + inicio.getIdEstudiante() + " ]" + " ->  ");
+                System.out.print(i + ".[ Estudiante: " + inicio.getNomEstudiante() + " ]" + " ->  ");
+                inicio = inicio.getSiguiente();
+                i++;
+            }
+            System.out.print( "NULL");
+        }
+    }
+    public String showCountStuds(){
+        return "La cantidad de estudiantes ingresados es: "+countEstuds+" estudiantes";
+    }
+    public void mostrarEstudiantes(){
+        int i=0;
+        if(!listaEstudsVacia()) {
+            while (inicio.getSiguiente() != null) {
+                System.out.print(i  +"--" +inicio.toString()+" " + " ->  ");
                 inicio = inicio.getSiguiente();
                 i++;
             }
@@ -115,7 +144,71 @@ public class Lista {
         }
     }
 
+    public boolean buscarId(int id){
+        Estudiante aux = inicio;// aux recorre la lista desde el inicio
+        boolean encontrado = false;
+        while(aux.getSiguiente() != null && !encontrado){
+            if (id == aux.getIdEstudiante()){
+                encontrado = true;
+                break;
+            }else{
+                // sino lo encontró entonces
+                // adelanta el "puntero" al siguiente estudiante
+                aux = aux.getSiguiente();
+            }
+        }
+        return encontrado;
+    }
+    public int positionEst(int id){
+        Estudiante aux = inicio;// aux recorre la lista desde el inicio
+        int index = 0;int i=0;
+        while(aux.getSiguiente() != null){
+            if (id == aux.getIdEstudiante()){
+                  index=i+1;
+                break;
+            }else{
+                // sino lo encontró entonces
+                // adelanta el "puntero" al siguiente estudiante
+                aux = aux.getSiguiente();
+                i++;
+            }
 
+        }
+        return index;
+    }
+    public void modificarEstudiante(){
+        System.out.println("\nIngrese el ID del estudiante a modificar : Int");
+        int idEstud = options.nextInt();
+        if (buscarId(idEstud)){
+            System.out.println("\n El ID Ingresado existe en lista ");
+            int pos= positionEst(idEstud);
+
+        }else{
+            System.out.println("\n El ID Ingresado NO existe en lista ");
+        }
+    }
+
+    public boolean buscarPorPosicion(int PosReferent){
+        // Crea una copia de la lista.
+        Estudiante aux = inicio;
+        // Bandera para indicar si el valor existe.
+        boolean encontrado = false;
+        // Recorre la lista hasta encontrar el elemento o hasta
+        // llegar al final de la lista.
+        while(aux != null && encontrado != true){
+            // Consulta si el valor del nodo es igual al de referencia.
+            if (PosReferent == countEstuds){
+                // Canbia el valor de la bandera.
+                encontrado = true;
+            }
+            else{
+                // Avansa al siguiente. nodo.
+                aux = aux.getSiguiente();
+            }
+        }
+        // Retorna el resultado de la bandera.
+        return encontrado;
+    }
 
 
 }
