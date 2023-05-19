f5e0c8a28b16837e21e2e461e76925ffb933dd7d
Minor Hernández <115409720+Minorhdez10@users.noreply.github.com>
2023-05-18 18:01:35 -0600

    Update Lista.java

--- src/Lista.java
+++ src/Lista.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Lista {
     Estudiante head;
+    Scanner scanner = new Scanner(System.in);
+    boolean finalizar = false;
     public static class Estudiante {
         private String nombre;
         private int idEstudiante;
@@ -84,30 +86,29 @@ public class Lista {
 
     public void ingresarEstudiante(Estudiante estudiante){
 
-        if(head == null)
-            head = estudiante;
+        if(head == null) //verifica si esta vacía
+            head = estudiante; //el nuevo objeto pasa a ser el primero de la lista
         else{
-            Estudiante ptr = head;
-            while(ptr.next!=null)
+            Estudiante ptr = head; //Si no está vacia, debe agregar al final de la lista.
+            while(ptr.next!=null) //recorre la lista hasta que sea nulo
                 ptr = ptr.next;
 
-            ptr.next = estudiante;
+            ptr.next = estudiante; //Apunta al nuevo objeto
         }
-
-
     }// end ingresarEstudiante
 
     public void mostrarListaDeEstudiantes(int opcion){
-        Estudiante ptr = head;
+        Estudiante ptr = head; //apunto al primer estudiante de la lista
+
 
         switch (opcion){
             case 2:
                 System.out.println("Lista de estudiantes por ID: ");
 
-                while(ptr.next!=null){
+                while(ptr.next!=null){ //verifica que no este vacía
                     if(ptr.getIdEstudiante() != 0) {
                         System.out.print(ptr.getIdEstudiante() + "-->");
-                        if(ptr.next != null && ptr.next.getIdEstudiante() != 0){
+                        if(ptr.next != null && ptr.next.getIdEstudiante() != 0){ //entre cada elemento de la lista se agrega uno 0 y lo que hace es ir quitandolos
                             System.out.print(" --> ");
                         }
                     }
@@ -118,10 +119,10 @@ public class Lista {
             case 3:
                 System.out.println("Lista de estudiantes por nombre: ");
 
-                while (ptr != null) {
+                while (ptr != null) { //verifica que no este vacía
                     if (ptr.getNombre() != null) {
                         System.out.print(ptr.getNombre()+ " --> ");
-                        if (ptr.next != null && ptr.next.getNombre() != null) {
+                        if (ptr.next != null && ptr.next.getNombre() != null) { //entre cada elemento de la lista se agrega uno nulo y lo que hace es ir quitandolos
                             System.out.print(" --> ");
                         }
                     }
@@ -133,9 +134,9 @@ public class Lista {
                 System.out.println("Lista de estudiantes con todos los datos: ");
 
                 while (ptr != null) {
-                    if (ptr.toString().contains("0") && ptr.toString().contains("null")) {
+                    if (ptr.toString().contains("0") && ptr.toString().contains("null")) { //entre cada elemento de la lista se agrega uno nulo o 0 y lo que hace es ir quitandolos
                     } else {
-                        System.out.print(ptr.toString());
+                        System.out.print(ptr.toString()); //toString contiene todos los datos
                         if (ptr.next != null) {
                             System.out.print(" -->");
                         }
@@ -144,18 +145,81 @@ public class Lista {
                 }
                 System.out.print("NULL\n\n");
                 break;
+            case 5:
+                boolean buscarID = false;
+                System.out.println("Ingrese el ID del estudiante para mostrar su información:");
+                int id = scanner.nextInt();
+
+                while (ptr!=null){
+                    if(ptr.getIdEstudiante() == id) { //Se busca uno que cooincida
+                        buscarID = true;
+                        System.out.println("La información del estudiante de acuerdo al ID " + id + " es: " + ptr.toString()+"\n\n");
+                    }
+                    ptr = ptr.next;
+                }
+                    if(!buscarID){
+                    System.out.println(id + " no existe en la lista"+"\n\n");
+                    }
+                break;
+            case 6:
+                boolean buscarNombre = false;
+                System.out.println("Ingrese el nombre del estudiante para mostrar su información:");
+                String nombre = scanner.nextLine();
+
+                while (ptr!=null) {
+                    if (ptr.getNombre() != null && ptr.getNombre().equals(nombre)) {
+                        buscarNombre = true;
+                        System.out.println("La información del estudiante " + nombre + " es: " + ptr.toString()+ "\n\n");
+                    }
+                    ptr = ptr.next;
+                }
+                    if(!buscarNombre){
+                        System.out.println(nombre + " no existe en la lista"+ "\n\n");
+                    }
+                break;
+            case 9:
+                System.out.println("Ingrese el nombre del estudiante que desea borrar");
+                String nombreBorrar = scanner.nextLine();
+
+                // Si el estudiante a eliminar está al principio de la lista
+                if (head.getNombre() != null && head.getNombre().equals(nombreBorrar)) {
+                    head = head.next;
+                    System.out.println("El estudiante " + nombreBorrar + " ha sido eliminado de la lista."+ "\n\n");
+                    return;
+                }
+
+                // Si el estudiante a eliminar está en otro lugar de la lista
+                Estudiante punteroAnterior = head;
+                Estudiante punteroActual = head.next;
+                while (punteroActual != null) {
+                    if (punteroActual.getNombre() != null && punteroActual.getNombre().equals(nombreBorrar)) {
+                        punteroAnterior.next = punteroActual.next;
+                        punteroActual = null;
+                        System.out.println("El estudiante " + nombreBorrar + " ha sido eliminado de la lista."+ "\n\n");
+                        return;
+                    }
+                    punteroAnterior = punteroActual;
+                    punteroActual = punteroActual.next;
+                }
+
+                // Si el estudiante no se encuentra en la lista
+                System.out.println(nombreBorrar + " no existe en la lista."+ "\n\n");
+                break;
         }
-
-
     }//end mostrarListaDeEstudiantes
 
     //Recibe un entero que significa la posición del estudiante que se desea obtener, y se recorrerá la lista hasta la posición ingresaada y se imprimirá el estudiante
     public void mostrarPorPosicion(int posicionBuscada){
         Estudiante ptr = head;
-        int contadorPosicion = 1;
+        int contadorPosicion = 1; //contador
+
+        //Si la lista está vacía, se imprime ese mensaje y se detiene el método con el return
+        if(ptr==null){
+            System.out.println("\nLa lista está vacia, debe ingresar un estudiante\n");
+        }
 
         while(ptr!=null){
-            if(contadorPosicion == posicionBuscada && ptr.getNombre() != null){
+            if(contadorPosicion == posicionBuscada && ptr.getNombre() != null){ // se compara la posición
                 System.out.println("El estudiante encontrado es: \n" + ptr.toString() + "\n");
                 break;
             }
@@ -169,20 +233,29 @@ public class Lista {
     //Recorre la lista hasta encontrar el estudiante que busca para cuando esto se haga, se modifique el dato correspondiente
     public void buscarEstudianteParaModificarDato(String nombre, int dato){
         Estudiante ptr = head;
+        boolean nombreEncontrado = false;
+
+        //Si la lista está vacía, se imprime ese mensaje y se detiene el método con el return
+        if(ptr==null){
+            System.out.println("\nLa lista está vacia, debe ingresar un estudiante\n");
+            this.finalizar = true;
+            return;
+        }
 
         while(ptr!=null){
             if(nombre.equalsIgnoreCase(ptr.getNombre())) {
                 modificarDato(dato, ptr);
                 break;
             }
-
             ptr = ptr.next;
         }//end while
+        if(!nombreEncontrado)
+            System.out.println("El nombre ingresado no existe en la lista."+ "\n\n");
+
     }//end buscarEstudianteParaModificarDato
 
     //modifica el dato que se solicite
     public void modificarDato(int dato,Estudiante ptr){
-        Scanner scanner = new Scanner(System.in);
 
         switch(dato){
             case 1:
@@ -212,10 +285,14 @@ public class Lista {
                 break;
         }//end switch
     }//end modificarDato
-
     public int totalEstudiantesRegistrados(){
         Estudiante ptr = head;
         int contadorEstudiantes = 0;
+
+        //Si la lista está vacía, se imprime ese mensaje y se detiene el método con el return
+        if(ptr==null){
+            System.out.println("\nLa lista está vacia, debe ingresar un estudiante\n");
+        }
 
         while(ptr!=null){
             if(ptr.getNombre() != null){ //Como cada vez que se ingresa un estudiante se agrega uno nulo en el medio de cada estudiante, si por el que pasa es nulo se lo salta
@@ -226,5 +303,6 @@ public class Lista {
 
         return contadorEstudiantes;
     }
-
 }//end class
+
+
